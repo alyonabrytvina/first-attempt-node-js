8fca2811ed31eea1c816977fe013e319ecbd032b
Alyona <alyona.ivanova.kh@gmail.com>
2022-08-09 20:32:35 +0300

    n

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,45 +1,45 @@
-const PostService = require("../PostService.js");
-
-//Контроллер обрабатывает запрос.
-//позволяют связать представления и модели и выполняют некоторую логику по обработке запроса.
-//Результат обработки контроллера отправляется в ответ клиенту.
-//Нередко ответ представляет html-страницу, которую пользователь видит в своем браузере.
-class PostController{
-    async getAll(req, res){
-        try{
-            const posts = await PostService.getAll();
-            return res.json(posts);
-        }catch (err){
-            res.status(500).json(err)
-        }
-    }
-
-    async getOne(req, res){
-        try{
-            const post = await PostService.getOne(req.params.id);
-            return res.json(post);
-        }catch (err){
-            res.status(500).json(err)
-        }
-    }
-
-    async update(req, res){
-        try{
-            const updatedPost = await PostService.update(req.body);
-            return res.json(updatedPost);
-        }catch (err){
-            res.status(500).json(err)
-        }
-    }
-
-    async delete(req, res){
-        try{
-            const updatedPost = await PostService.update(req.params.id);
-            return res.json(updatedPost);
-        }catch (err){
-            res.status(500).json(err)
-        }
-    }
-}
-
-module.exports = new PostController();
+// const PostService = require("../PostService.js");
+//
+// //Контроллер обрабатывает запрос.
+// //позволяют связать представления и модели и выполняют некоторую логику по обработке запроса.
+// //Результат обработки контроллера отправляется в ответ клиенту.
+// //Нередко ответ представляет html-страницу, которую пользователь видит в своем браузере.
+// class PostController{
+//     async getAll(req, res){
+//         try{
+//             const posts = await PostService.getAll();
+//             return res.json(posts);
+//         }catch (err){
+//             res.status(500).json(err)
+//         }
+//     }
+//
+//     async getOne(req, res){
+//         try{
+//             const post = await PostService.getOne(req.params.id);
+//             return res.json(post);
+//         }catch (err){
+//             res.status(500).json(err)
+//         }
+//     }
+//
+//     async update(req, res){
+//         try{
+//             const updatedPost = await PostService.update(req.body);
+//             return res.json(updatedPost);
+//         }catch (err){
+//             res.status(500).json(err)
+//         }
+//     }
+//
+//     async delete(req, res){
+//         try{
+//             const updatedPost = await PostService.update(req.params.id);
+//             return res.json(updatedPost);
+//         }catch (err){
+//             res.status(500).json(err)
+//         }
+//     }
+// }
+//
+// module.exports = new PostController();
--- modelsPost.js
+++ modelsPost.js
@@ -1,45 +1,45 @@
-const mongoose = require("mongoose");
-
-//Mongoose - это ODM (* Object Document Mapper - объектно-документный отобразитель).
-//Это означает, что Mongoose позволяет вам определять объекты со
-//строго-типизированной схемой, соответствующей документу MongoDB.
-const ModelsPost = new mongoose.Schema({
-    data: {
-        "taskName": String,
-        "developers": [String],
-        "workType": String,
-        "status": String,
-        "estimation": Number,
-        "totalTimeSpent": Number,
-        "myTimeSpentByPeriod": Number,
-        "efficiency": Boolean,
-    },
-    "status": [String],
-    "developers": {
-        "design": [String],
-        "frontEnd": [String],
-        "backEnd": [String],
-        "pms": [String]
-    },
-    "selectCategories": [
-        {
-            "value": String,
-            "label": String
-        },
-    ],
-    "paginationSelect": [
-        {
-            "option": Number,
-        },
-    ]
-})
-
-//Затем, используя эту схему, создаем модель пользователя:
-//Mongoose затем будет автоматически искать в базе данных коллекцию,
-//название которой соответствует названию модели во множественном числе.
-//Например, в данном случае название модели "ModelsPost".
-//Во множественном числе это "posts".
-//Поэтому при работе с данными модели ModelsPost (добавлении, удалении, редактировании и получении объектов)
-//mongoose будет обращаться к коллекции "modelsPosts"
-//Второй параметр функции mongoose.model - собственно схема.
-module.exports = mongoose.model('ModelsPost', ModelsPost)
+// const mongoose = require("mongoose");
+//
+// //Mongoose - это ODM (* Object Document Mapper - объектно-документный отобразитель).
+// //Это означает, что Mongoose позволяет вам определять объекты со
+// //строго-типизированной схемой, соответствующей документу MongoDB.
+// const ModelsPost = new mongoose.Schema({
+//     data: {
+//         "taskName": String,
+//         "developers": [String],
+//         "workType": String,
+//         "status": String,
+//         "estimation": Number,
+//         "totalTimeSpent": Number,
+//         "myTimeSpentByPeriod": Number,
+//         "efficiency": Boolean,
+//     },
+//     "status": [String],
+//     "developers": {
+//         "design": [String],
+//         "frontEnd": [String],
+//         "backEnd": [String],
+//         "pms": [String]
+//     },
+//     "selectCategories": [
+//         {
+//             "value": String,
+//             "label": String
+//         },
+//     ],
+//     "paginationSelect": [
+//         {
+//             "option": Number,
+//         },
+//     ]
+// })
+//
+// //Затем, используя эту схему, создаем модель пользователя:
+// //Mongoose затем будет автоматически искать в базе данных коллекцию,
+// //название которой соответствует названию модели во множественном числе.
+// //Например, в данном случае название модели "ModelsPost".
+// //Во множественном числе это "posts".
+// //Поэтому при работе с данными модели ModelsPost (добавлении, удалении, редактировании и получении объектов)
+// //mongoose будет обращаться к коллекции "modelsPosts"
+// //Второй параметр функции mongoose.model - собственно схема.
+// module.exports = mongoose.model('ModelsPost', ModelsPost)
