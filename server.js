11aa624891438484281f39ca19bfe3d714a4330e
Alyona <alyona.ivanova.kh@gmail.com>
2022-08-10 09:56:10 +0300

    r

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const data = require("./db.js");
+
 const jsonServer = require("json-server");
-
 const server = jsonServer.create();
-const router = jsonServer.router(data);
 
+const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+});
 
 
 
